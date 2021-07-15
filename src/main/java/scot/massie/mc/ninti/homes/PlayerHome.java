20aac88954281b61a5204d734498874635506fe7
Chris Massie <chris@massie.scot>
2021-07-15 02:12:13 +0100

    Implemented PlayerHome
    Implemented PlayerHome

--- src/main/java/scot/massie/mc/ninti/homes/PlayerHome.java
+++ src/main/java/scot/massie/mc/ninti/homes/PlayerHome.java
@@ -1,12 +1,34 @@
 package scot.massie.mc.ninti.homes;
 
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.common.UsernameCache;
+import scot.massie.lib.collections.maps.DoubleMap;
+import scot.massie.lib.collections.maps.MapUtils;
+import scot.massie.lib.maths.Equation;
+import scot.massie.mc.ninti.core.Permissions;
+import scot.massie.mc.ninti.core.PluginUtils;
+import scot.massie.mc.ninti.core.currencies.Currencies;
 import scot.massie.mc.ninti.core.utilclasses.EntityLocation;
+import scot.massie.mc.ninti.core.zones.Zone;
+import scot.massie.mc.ninti.core.zones.Zones;
 
+import static scot.massie.lib.utils.StringUtils.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class PlayerHome
 {
+    final static Equation.Builder tpCostEqBuilder
+            = new Equation.Builder()
+                      .withComparativeOperators()
+                      .withVariable("distance", 0)
+                      .withVariable("isAcrossWorlds", 0);
+
     final UUID playerId;
     final String homeName;
     final EntityLocation location;
@@ -19,22 +41,184 @@ public class PlayerHome
     }
 
     public EntityLocation getLocation()
-    {
-        return location;
-    }
+    { return location; }
+
+    public ServerPlayerEntity getPlayer()
+    { return PluginUtils.getServer().getPlayerList().getPlayerByUUID(playerId); }
+
+    public String getPlayerName()
+    { return UsernameCache.getLastKnownUsername(playerId); }
 
     public boolean playerHasPermissionToTpHere()
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        PlayerEntity onlineOwner = getPlayer();
+
+        if(onlineOwner != null)
+        {
+            EntityLocation playerLocation = new EntityLocation(onlineOwner);
+            String fromWorldPermission = NintiHomes.PERMISSION_HOMES_TP_FROMWORLD + "." + playerLocation.getWorldId();
+
+            if(!Permissions.playerHasPermission(playerId, fromWorldPermission))
+                return false;
+
+            for(Zone z : Zones.getZonesAt(playerLocation))
+            {
+                String fromZonePermission = NintiHomes.PERMISSION_HOMES_TP_FROMZONE + "." + z.getName();
+
+                if(!Permissions.playerHasPermission(playerId, fromZonePermission))
+                    return false;
+            }
+        }
+
+        String toWorldPermission = NintiHomes.PERMISSION_HOMES_TP_TOWORLD + "." + location.getWorldId();
+
+        if(!Permissions.playerHasPermission(playerId, toWorldPermission))
+            return false;
+
+        for(Zone z : Zones.getZonesAt(location))
+        {
+            String toZonePermission = NintiHomes.PERMISSION_HOMES_TP_TOZONE + "." + z.getName();
+
+            if(!Permissions.playerHasPermission(playerId, toZonePermission))
+                return false;
+        }
+
+        return true;
     }
 
     public Map<String, Double> getCostsToTpHere()
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        PlayerEntity onlineOwner = getPlayer();
+        Collection<Map<String, Double>> resultParts = new ArrayList<>();
+
+        if(onlineOwner != null)
+        {
+            EntityLocation playerLocation = new EntityLocation(onlineOwner);
+            String fromWorldPermission = NintiHomes.PERMISSION_HOMES_TP_FROMWORLD + "." + playerLocation.getWorldId();
+            resultParts.add(getTpCostsFromPerm(fromWorldPermission));
+
+            for(Zone z : Zones.getZonesAt(playerLocation))
+            {
+                String fromZonePermission = NintiHomes.PERMISSION_HOMES_TP_FROMZONE + "." + z.getName();
+                resultParts.add(getTpCostsFromPerm(fromWorldPermission));
+            }
+        }
+
+        String toWorldPermission = NintiHomes.PERMISSION_HOMES_TP_TOWORLD + "." + location.getWorldId();
+        resultParts.add(getTpCostsFromPerm(toWorldPermission));
+
+        for(Zone z : Zones.getZonesAt(location))
+        {
+            String toZonePermission = NintiHomes.PERMISSION_HOMES_TP_TOZONE + "." + z.getName();
+            resultParts.add(getTpCostsFromPerm(toWorldPermission));
+        }
+
+        return MapUtils.sumMatchingDoubleValues(resultParts);
     }
 
-    public void tpPlayerHere()
+    public Map<String, Double> getTpCostsFromPerm(String permission)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        String arg = Permissions.getPlayerPermissionArg(playerId, permission);
+
+        if(arg == null)
+            return new HashMap<>();
+
+        Map<String, String> tpCostEquationsAsStrings = splitColonSeparatedValuePairs(arg);
+        DoubleMap<String> tpCosts = new DoubleMap<>();
+
+        PlayerEntity onlineOwner = getPlayer();
+        EntityLocation onlineOwnerLocation = onlineOwner == null ? null : new EntityLocation(onlineOwner);
+        double playerDistance = onlineOwner == null ? 0 : onlineOwnerLocation.getDistanceTo(location);
+        // 1 == true, 0 == false
+        double tpIsAcrossWorlds
+                = ((onlineOwner == null) || onlineOwnerLocation.getWorldId().equals(location.getWorldId())) ? 0 : 1;
+
+        for(Map.Entry<String, String> e : tpCostEquationsAsStrings.entrySet())
+        {
+            if(e.getValue() == null)
+            {
+                System.err.println("Currency cost improperly formatted: " + e.getKey()
+                                   + "\n    Should be in the form of, without square brackets: "
+                                   + "[currency code]: [equation]");
+                continue;
+            }
+
+            Equation eq;
+            try
+            { eq = tpCostEqBuilder.build(e.getValue()); }
+            catch(Equation.Builder.EquationParseException ex)
+            {
+                System.err.println("Error parsing currency cost for " + e.getKey() + ": " + e.getValue() + ":\n\n"
+                                   + ex.getMessage().replaceAll("(?m)^", "    "));
+
+                continue;
+            }
+
+            eq.setVariable("distance", playerDistance);
+            eq.setVariable("isAcrossWorlds", tpIsAcrossWorlds);
+
+            tpCosts.add(e.getKey(), eq.evaluate());
+        }
+
+        return tpCosts;
+    }
+
+    public void tpHere()
+    {
+        ServerPlayerEntity player = getPlayer();
+
+        if(player == null)
+        {
+            String playerName = UsernameCache.getLastKnownUsername(playerId);
+
+            if(playerName == null)
+                playerName = playerId.toString();
+
+            System.err.println("Attempted to teleport offline player: " + playerName);
+            return;
+        }
+
+        location.tpPlayerToHere(player);
+    }
+
+    public void tpHere(ServerPlayerEntity player)
+    { location.tpPlayerToHere(player); }
+
+    public void requestTpHere()
+    {
+        ServerPlayerEntity player = getPlayer();
+
+        if(!playerHasPermissionToTpHere())
+        {
+            if(player != null)
+                PluginUtils.sendMessage(player, "You do not have permission to teleport to that home.");
+
+            return;
+        }
+
+        Map<String, Double> tpCosts = getCostsToTpHere();
+        boolean couldAfford;
+
+        try
+        { couldAfford = Currencies.chargePlayer(playerId, tpCosts); }
+        catch(Currencies.UnrecognisedCurrencyException e)
+        {
+            if(player != null)
+                PluginUtils.sendMessage(player, "Could not teleport - unrecognised currency: " + e.getCurrencyName());
+
+            System.err.println("Unrecognised currency: " + e.getCurrencyName());
+            return;
+        }
+
+        if((!couldAfford) && (player != null))
+        {
+            StringBuilder msg = new StringBuilder("You cannot afford to teleport to that home. It would cost:");
+
+            for(Map.Entry<String, Double> e : tpCosts.entrySet())
+                msg.append(" - ").append(e.getKey()).append(": ").append(e.getValue());
+
+            PluginUtils.sendMessage(player, msg.toString());
+        }
     }
 }
+
