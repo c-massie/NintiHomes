0851999d151e995c1ec73af4081b53404b06480c
Chris Massie <chris@massie.scot>
2021-07-16 20:12:00 +0100

    Implemented PlayerHomesList
    Implemented PlayerHomesList.
    
    Made PlayerHomesList.clear return void instead of PlayerHome.
    
    Made PlayerHomesList final.

--- src/main/java/scot/massie/mc/ninti/homes/PlayerHomesList.java
+++ src/main/java/scot/massie/mc/ninti/homes/PlayerHomesList.java
@@ -1,13 +1,19 @@
 package scot.massie.mc.ninti.homes;
 
+import net.minecraft.entity.player.ServerPlayerEntity;
+import scot.massie.mc.ninti.core.Permissions;
+import scot.massie.mc.ninti.core.PluginUtils;
 import scot.massie.mc.ninti.core.utilclasses.EntityLocation;
+import scot.massie.mc.ninti.core.zones.Zone;
+import scot.massie.mc.ninti.core.zones.Zones;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class PlayerHomesList
+public final class PlayerHomesList
 {
     final UUID playerId;
     final Map<String, PlayerHome> playerHomes = new HashMap<>();
@@ -19,41 +25,91 @@ public class PlayerHomesList
 
     public PlayerHome getHome(String homeName)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        synchronized(playerHomes)
+        { return playerHomes.get(homeName); }
     }
 
     public List<PlayerHome> getHomes()
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        synchronized(playerHomes)
+        { return new ArrayList<>(playerHomes.values()); }
+    }
+
+    public Map<String, PlayerHome> getHomesAsMap()
+    {
+        synchronized(playerHomes)
+        { return new HashMap<>(playerHomes); }
     }
 
     public PlayerHome setHome(String homeName)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        ServerPlayerEntity player = PluginUtils.getOnlinePlayer(playerId);
+
+        if(player == null)
+            return null;
+
+        return setHome(homeName, new EntityLocation(player));
     }
 
     public PlayerHome setHome(String homeName, EntityLocation location)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        PlayerHome newHome = new PlayerHome(playerId, homeName, location);
+
+        synchronized(playerHomes)
+        { playerHomes.put(homeName, newHome); }
+
+        return newHome;
     }
 
     public PlayerHome requestSetHome(String homeName)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        ServerPlayerEntity player = PluginUtils.getOnlinePlayer(playerId);
+
+        if(player == null)
+            return null;
+
+        return requestSetHome(homeName, new EntityLocation(player));
     }
 
     public PlayerHome requestSetHome(String homeName, EntityLocation location)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        final String noPermissionMessage = "You do not have permission to set a home there.";
+        ServerPlayerEntity player = PluginUtils.getOnlinePlayer(playerId);
+        String inWorldPermission = NintiHomes.PERMISSION_HOMES_ADD_INWORLD + "." + location.getWorldId();
+
+        if(!Permissions.playerHasPermission(playerId, inWorldPermission))
+        {
+            if(player != null)
+                PluginUtils.sendMessage(player, noPermissionMessage);
+
+            return null;
+        }
+
+        for(Zone z : Zones.getZonesAt(location))
+        {
+            String inZonePermission = NintiHomes.PERMISSION_HOMES_ADD_INZONE + "." + z.getName();
+
+            if(!Permissions.playerHasPermission(playerId, inZonePermission))
+            {
+                if(player != null)
+                    PluginUtils.sendMessage(player, noPermissionMessage);
+
+                return null;
+            }
+        }
+
+        return setHome(homeName, location);
     }
 
     public PlayerHome deleteHome(String homeName)
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        synchronized(playerHomes)
+        { return playerHomes.remove(homeName); }
     }
 
-    public PlayerHome clear()
+    public void clear()
     {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        synchronized(playerHomes)
+        { playerHomes.clear(); }
     }
 }
